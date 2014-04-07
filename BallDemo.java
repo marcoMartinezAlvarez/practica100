a6f98dc53efd3a300bc611ccce9e57a6e985ed1f
Marco <marco.martinez.alvarez@gmail.com>
2014-04-07 10:35:55 +0200

    finalizado la clase bounce para las bolas y testeado

--- BallDemo.java
+++ BallDemo.java
@@ -13,7 +13,7 @@ public class BallDemo
 {
     private Canvas myCanvas;
     private Random aleatorio;
-   
+
     /**
      * Create a BallDemo object. Creates a fresh canvas and makes it visible.
      */
@@ -30,44 +30,38 @@ public class BallDemo
         int ground = 400;   // position of the ground line
         ArrayList <BouncingBall> bolas = new ArrayList <>();
         myCanvas.setVisible(true);
-        
 
         // draw the ground
         myCanvas.drawLine(50, ground, 550, ground);
 
         // crate and show the balls
         for(int i=0; i < nbolas; i++){
-            aleatorio = new Random();
+            Random aleatorio = new Random();
             int red = aleatorio.nextInt(250);
             int blue = aleatorio.nextInt(250);
             int green = aleatorio.nextInt(250);
-            int x = aleatorio.nextInt(250);
-            int y = aleatorio.nextInt(250);
-            
-            add.bolas(250);
-            
-        
-        
-        }
-        
-        BouncingBall ball = new BouncingBall(50, 50, 16, Color.BLUE, ground, myCanvas);
-        ball.draw();
-        BouncingBall ball2 = new BouncingBall(70, 80, 20, Color.RED, ground, myCanvas);
-        ball2.draw();
-        
-        
+            int x = aleatorio.nextInt(240);
+            int y = aleatorio.nextInt(400);
+            int sizeRadio = aleatorio.nextInt(100);
 
-        // make them bounce
-        
-        boolean finished =  false;
-        while(!finished) {
-            myCanvas.wait(50);           // small delay
-            ball.move();
-            ball2.move();
-            // stop once ball has travelled a certain distance on x axis
-            if(ball.getXPosition() >= 550 || ball2.getXPosition() >= 550) {
-                finished = true;
-            }
+            BouncingBall ball = new BouncingBall(x,y, sizeRadio,new Color(blue,red,green), ground, myCanvas);
+            bolas.add(ball);
+            ball.draw();
+
         }
+            // make them bounce
+            boolean finished =  false;
+            while(!finished) {
+                myCanvas.wait(50);
+                for(BouncingBall pelota : bolas){
+                    pelota.move();
+                    
+                    // stop once ball has travelled a certain distance on x axis
+                    if(pelota.getXPosition() >= 550 ) {
+                        finished = true;
+                    }
+                }
+            }
+        
     }
 }
