ae0c75eab1362954fb2e7dcd18e0217df342b3d3
Marco <marco.martinez.alvarez@gmail.com>
2014-04-07 09:55:11 +0200

    creado un for para crear mas bolas y dentro utilizar la clse Random

--- BallDemo.java
+++ BallDemo.java
@@ -1,5 +1,6 @@
 import java.awt.Color;
-
+import java.util.ArrayList;
+import java.util.Random;
 /**
  * Class BallDemo - a short demonstration showing animation with the 
  * Canvas class. 
@@ -11,7 +12,8 @@ import java.awt.Color;
 public class BallDemo   
 {
     private Canvas myCanvas;
-
+    private Random aleatorio;
+   
     /**
      * Create a BallDemo object. Creates a fresh canvas and makes it visible.
      */
@@ -23,22 +25,40 @@ public class BallDemo
     /**
      * Simulate two bouncing balls
      */
-    public void bounce()
+    public void bounce(int nbolas)
     {
         int ground = 400;   // position of the ground line
-
+        ArrayList <BouncingBall> bolas = new ArrayList <>();
         myCanvas.setVisible(true);
+        
 
         // draw the ground
         myCanvas.drawLine(50, ground, 550, ground);
 
         // crate and show the balls
+        for(int i=0; i < nbolas; i++){
+            aleatorio = new Random();
+            int red = aleatorio.nextInt(250);
+            int blue = aleatorio.nextInt(250);
+            int green = aleatorio.nextInt(250);
+            int x = aleatorio.nextInt(250);
+            int y = aleatorio.nextInt(250);
+            
+            add.bolas(250);
+            
+        
+        
+        }
+        
         BouncingBall ball = new BouncingBall(50, 50, 16, Color.BLUE, ground, myCanvas);
         ball.draw();
         BouncingBall ball2 = new BouncingBall(70, 80, 20, Color.RED, ground, myCanvas);
         ball2.draw();
+        
+        
 
         // make them bounce
+        
         boolean finished =  false;
         while(!finished) {
             myCanvas.wait(50);           // small delay
